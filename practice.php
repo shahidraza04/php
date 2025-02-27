aa134377a1ab733b66eb439bd649ec15382f4a1b
Md. Shahid Raza <shahidhets1@gmail.com>
2025-02-27 11:41:16 +0530

    Create practice.php

---
+++ practice.php
@@ -0,0 +1,22 @@
+<?php
+echo"
+<html>
+<head><title>practice in php</title>/head>
+<body>
+<h1>Hello World</h1></br>
+<i>italic font tag</i></br>
+<b>bold font tag</b></br>
+<u>underline tag</u></br>
+<em>emphasized tag</em></br>
+<strong>important</strong></br>
+<mark>marked</mark></br>
+<small>smaller tag</small></br>
+<big>bigger text tag</big></br>
+<ins>insted tag</ins></br>
+<del>deleted tag</del></br>
+<sup>superscript</sup></br>
+<sub>subscript</sub></br>
+</body>
+</html>
+";
+?>
