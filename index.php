37e194b53ffba99dae5926020523ef65d28bbb83
Md. Shahid Raza <shahidhets1@gmail.com>
2025-02-27 11:40:48 +0530

    Create index.php

---
+++ index.php
@@ -0,0 +1,21 @@
+<?php
+//echo : used to display string / text output on webpage.
+echo"shahid raza";
+echo "
+<html>
+<head>
+<title>php project</title>
+</head>
+<body>
+<h1>welcome to php</h1>
+<h2>hellow world</h>
+<h3>php</h3>
+<p>You have successfully installed XAMPP on this system! Now you can start using Apache, MariaDB, PHP and other components. You can find more info in the FAQs section or check the HOW-TO Guides for getting started with PHP applications.
+
+XAMPP is meant only for development purposes. It has certain configuration settings that make it easy to develop locally but that are insecure if you want to have your installation accessible to others.
+
+Start the XAMPP Control Panel to check the server status.</p>
+</body>
+</html>
+";
+?>
